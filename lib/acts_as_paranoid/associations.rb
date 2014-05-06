51a80dbcbeeac70787df66e683fe6a1cbdc26bc7
Pavel Guseynov <pavel.guseynov@gmail.com>
2014-05-06 17:44:55 +0400

    for test

--- lib/acts_as_paranoid/associations.rb
+++ lib/acts_as_paranoid/associations.rb
@@ -9,7 +9,7 @@ module ActsAsParanoid
 
     module ClassMethods
       def belongs_to_with_deleted(target, scope = nil, options = {})
-        with_deleted = (scope.is_a?(Hash) ? scope : options).delete(:with_deleted)
+        with_deleted = options.delete(:with_deleted)
         result = belongs_to_without_deleted(target, scope, options)
 
         if with_deleted
